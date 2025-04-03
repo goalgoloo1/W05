d3f8dc1690f50982a384f50836c7338359297c7e
Vomeee <158576443+Vomeee@users.noreply.github.com>
2025-04-03 17:06:44 +0900

    카메라 3인칭 follow 2개로 변경. 아직 조금 부자연스러움

--- Assets/HW/Scripts/CameraFollowPointBehaviour.cs
+++ Assets/HW/Scripts/CameraFollowPointBehaviour.cs
@@ -1,17 +1,16 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class CameraFollowPointBehaviour : MonoBehaviour
 {
     private void LateUpdate()
     {
-        // 메인 카메라의 회전 가져오기
-        Quaternion cameraRotation = Camera.main.transform.rotation;
-        Vector3 cameraEulerAngles = cameraRotation.eulerAngles;
 
-        // X축 회전을 -50도 ~ 50도로 제한
-        float clampedX = Mathf.Clamp(cameraEulerAngles.x, -50f, 50f);
+            Quaternion mainCameraRotation = Camera.main.transform.rotation;
+            Vector3 mainCameraEulerAngle = mainCameraRotation.eulerAngles;
 
-        // 제한된 X축과 원래의 Y, Z축으로 새 회전 생성
-        transform.rotation = Quaternion.Euler(clampedX, cameraEulerAngles.y, cameraEulerAngles.z);
+            transform.rotation = Quaternion.Euler(mainCameraEulerAngle);
+       
+
     }
 }
