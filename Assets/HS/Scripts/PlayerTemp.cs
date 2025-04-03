e020cb6b9878306050afb28ba27f95fdfd004e4f
goalgoloo1 <goalgoloo1@gmail.com>
2025-04-03 21:05:12 +0900

    add TempPlayer

---
+++ Assets/HS/Scripts/PlayerTemp.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerTemp : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+
+    private Rigidbody rb;
+    private Vector3 moveInput;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        if (rb == null)
+        {
+            Debug.LogError("플레이어 오브젝트에 Rigidbody 컴포넌트가 없습니다!", gameObject);
+        }
+    }
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        moveInput = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        if (rb == null) return;
+
+        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
+    }
+}
